6363091361219bfd49f2b57ac64d93bf5c8959af
goldnmean <goldnmean@me.com>
2016-05-31 20:37:52 -0600

    Init commit: create initial elevator and controller classes

---
+++ elevator.js
@@ -0,0 +1,10 @@
+function Elevator () {
+  // 2. Each elevator will report as it moves from floor to floor.
+  // 3. Each elevator will report when it opens or closes its doors.
+  // 4. An elevator cannot proceed above the top floor.
+  // 5. An elevator cannot proceed below the ground floor (assume 1 as the min).
+  // 6. An elevator request can be made at any floor, to go to any other floor.
+  /* 8. The elevator should keep track of how many trips it has made, and how many floors it has passed.
+  The elevator should go into maintenance mode after 100 trips, and stop functioning until serviced,
+  therefore not be available for elevator calls.*/
+}
---
+++ elevatorController.js
@@ -0,0 +1,11 @@
+function ElevatorController () {
+  /* 1. Initialize the elevator simulation with the desired number of elevators, and the desired number of floors.
+        Assume ground/min of 1. */
+
+  // 6. An elevator request can be made at any floor, to go to any other floor.
+
+  /* 7.  When an elevator request is made, the unoccupied elevator closest to it will answer
+        the call, unless an occupied elevator is moving and will pass that floor on its way.
+        The exception is that if an unoccupied elevator is already stopped at that floor,
+        then it will always have the highest priority answering that call. */
+};
